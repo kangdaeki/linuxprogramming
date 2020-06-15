26d4a8a7e05fb894b47bdd6c08ffb7a9c7006b97
kangdaeki <dkkang@gmail.com>
2020-06-15 19:57:13 +0900

    Add files via upload

---
+++ chat_client.c
@@ -0,0 +1,75 @@
+#define _REENTRANT
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+void error_handling(char *mesg)
+{
+	fprintf(stderr, "PANIC: %s.\n", mesg);
+	exit(1);
+}
+
+#define NAME_SIZE 1024
+char name[NAME_SIZE]="[DEFAULT]";
+#define BUF_SIZE 1024
+char msg[BUF_SIZE]={};
+
+void *send_msg(void *arg)
+{
+	int sock=*((int*)arg);
+	char name_msg[NAME_SIZE+BUF_SIZE]={};
+	while (1) {
+		fgets(msg, BUF_SIZE, stdin);
+		if (!strcmp(msg,"q\n")||!strcmp(msg,"Q\n")) {
+			close(sock);
+			exit(1);
+		}
+		sprintf(name_msg, "%s %s", name, msg);
+		write(sock, name_msg, strlen(name_msg));
+	}
+	return NULL;
+}
+
+void *recv_msg(void *arg)
+{
+	int sock=*((int*)arg);
+	char name_msg[NAME_SIZE+BUF_SIZE]={};
+	while (1) {
+		int str_len=read(sock, name_msg, NAME_SIZE+BUF_SIZE-1);
+		if (-1==str_len) return (void*)-1;
+		name_msg[str_len]=0;
+		fputs(name_msg,stdout);
+	}
+	return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+	if (4!=argc) {
+		fprintf(stdout, "USAGE: %s <IP> <port> <name>\n", argv[0]);
+		return 1;
+	}
+	sprintf(name, "[%s]", argv[3]);
+	int sock=socket(PF_INET, SOCK_STREAM, 0);
+	if (-1==sock) error_handling("cannot server socket()");
+	struct sockaddr_in serv_addr;
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family=AF_INET;
+	serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
+	serv_addr.sin_port=htons(atoi(argv[2]));
+	if (-1==connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr))) error_handling("cannot connect()");
+	pthread_t snd_thread, rcv_thread;
+	pthread_create(&snd_thread, NULL, send_msg, (void*)&sock);
+	pthread_create(&rcv_thread, NULL, recv_msg, (void*)&sock);
+	void *thread_return;
+	pthread_join(snd_thread,&thread_return);
+	pthread_join(rcv_thread,&thread_return);
+	close(sock);
+	return 0;
+}
+
---
+++ chat_server.c
@@ -0,0 +1,89 @@
+#define _REENTRANT
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <arpa/inet.h>
+
+void error_handling(char *mesg)
+{
+	fprintf(stderr, "PANIC: %s.\n", mesg);
+	exit(1);
+}
+
+int clnt_cnt=0;
+pthread_mutex_t mutx;
+#define MAX_CLNT 256
+int clnt_socks[MAX_CLNT];
+
+void send_msg(char *msg, int len)
+{
+	pthread_mutex_lock(&mutx);
+	for (int i=0;i<clnt_cnt;i++) write(clnt_socks[i],msg,len);
+	pthread_mutex_unlock(&mutx);
+}
+
+#define BUF_SIZE 1024
+
+void *handle_clnt(void *arg)
+{
+	int clnt_sock=*((int*)arg);
+	int str_len=0;
+	char msg[BUF_SIZE]={};
+	while (0!=(str_len=read(clnt_sock, msg, sizeof(msg)))) send_msg(msg, str_len);
+
+	// remove disconnected client
+	pthread_mutex_lock(&mutx);
+	for (int i=0;i<clnt_cnt;i++) {
+		if (clnt_sock==clnt_socks[i]) {
+			for (int j=i;j<clnt_cnt-1;j++) clnt_socks[j]=clnt_socks[j+1];
+			break;
+		}
+	}
+	clnt_cnt--;
+	pthread_mutex_unlock(&mutx);
+	close(clnt_sock);
+	return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+	if (2!=argc) {
+		fprintf(stdout, "USAGE: %s <port>\n", argv[0]);
+		return 1;
+	}
+	int serv_sock=socket(PF_INET, SOCK_STREAM, 0);
+	if (-1==serv_sock) error_handling("cannot server socket()");
+	struct sockaddr_in serv_adr;
+	memset(&serv_adr, 0, sizeof(serv_adr));
+	serv_adr.sin_family=AF_INET;
+	serv_adr.sin_addr.s_addr=htonl(INADDR_ANY);
+	serv_adr.sin_port=htons(atoi(argv[1]));
+	if (-1==bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr))) error_handling("cannot bind()");
+	if (-1==listen(serv_sock,5)) error_handling("cannot listen()");
+	pthread_mutex_init(&mutx, NULL);
+	while (1) {
+		struct sockaddr_in clnt_adr;
+		socklen_t clnt_adr_sz=sizeof(clnt_adr);
+		int clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
+		if (-1==clnt_sock) {
+			fprintf(stderr, "cannot client sock()\n");
+			continue;
+		}
+		pthread_mutex_lock(&mutx);
+		clnt_socks[clnt_cnt]=clnt_sock;
+		clnt_cnt++;
+		pthread_mutex_unlock(&mutx);
+		pthread_t t_id;
+		pthread_create(&t_id, NULL, handle_clnt, (void *)&clnt_sock);
+		pthread_detach(t_id);
+		fprintf(stdout, "Connected client IP: %s\n", inet_ntoa(clnt_adr.sin_addr));
+	}
+	close(serv_sock);
+	return 0;
+}
+
